2833ba2a25adcde14cb3c2ad76c6ea7229a35b24
EC2 Default User <ec2-user@ip-10-10-3-143.ap-northeast-1.compute.internal>
2019-10-08 01:38:56 +0000

    go pull req

--- main.js
+++ main.js
@@ -10,10 +10,32 @@
 //     - https://tsuyopon.xyz/learning-contents/web-dev/javascript/frontend/how-to-use-the-fetch-api-in-js/
 //   - MDN : fetch
 //     - https://developer.mozilla.org/ja/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+const END_POINT = 'https://opentdb.com/api.php?';
 
+class QuizFetcher{
+    static fetchMultipleQuizData(){
+        const url = `${END_POINT}amount=10&type=multiple`;
+        return fetch(url)
+            .then(responce => {
+                return responce.json();
+            });
+    }
+    static fetchTrueOrFalseQuizData(){
+        const url = `${END_POINT}amount=10&type=boolean`;
+        return fetch(url).then(responce => responce.json());
+    }
+}
 
 // 課題2: fetchMultipleQuizDataメソッドを実行してAPIの結果に含まれるresultsプロパティの値をconsole.logで出力する
+QuizFetcher.fetchMultipleQuizData()
+    .then(data => {
+        console.log(data.results);
+    });
 
 
 // 課題3: fetchTrueOrFalseQuizDataメソッドを実行してAPIの結果に含まれるresultsプロパティの値をconsole.logで出力する
-
+QuizFetcher
+    .fetchTrueOrFalseQuizData()
+    .then(data => {
+        console.log(data.results);
+    });
